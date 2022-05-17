7318767f3eafd564821c6a5c6a5d16b3880a9c29
Abhishek Sharma <sharma.abhishek208@gmail.com>
2022-05-17 10:13:29 +0000

    intialized todoapp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import './App.css';
 import React, {Component} from 'react';
 
-// import FirstComponent from './components/learning-examples/FirstComponent';
-// import SecondComponent from './components/learning-examples/SecondComponent';
-// import ThirdComponent from './components/learning-examples/ThirdComponent';
-
 import Counter from './components/counter/counter';
+import TodoApp from './components/todo/TodoApp'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter/>
+        {/* <Counter/> */}
+        <TodoApp/>
       </div>
     );
   }
 }
 
-// class LearningComponent extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <FirstComponent />
-//         <SecondComponent> </SecondComponent>
-//         <ThirdComponent/>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoApp extends Component {
+    render() {
+        return (
+            <div className='TodoApp'>
+                My Todo App
+            </div>
+        )
+    }
+}
+
+export default TodoApp
