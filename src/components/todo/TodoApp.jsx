6a1ae6b5e1b49f54b47645507340395816b898c1
Abhishek Sharma <sharma.abhishek208@gmail.com>
2022-05-17 10:44:42 +0000

    added login component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -4,8 +4,38 @@ class TodoApp extends Component {
     render() {
         return (
             <div className='TodoApp'>
-                My Todo App
+                <LoginComponent/>
             </div>
+        )
+    }
+}
+
+class LoginComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: "in28minutes",
+            password: ""
+        }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    }
+
+    handleUsernameChange(event) {
+        this.setState({username:event.target.value})
+    }
+    handlePasswordChange(event) {
+        this.setState({password:event.target.value})
+    }
+
+    render() {
+        return (
+            <>
+            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange} />
+            Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+            <button>Login</button>
+            </>
+            
         )
     }
 }
